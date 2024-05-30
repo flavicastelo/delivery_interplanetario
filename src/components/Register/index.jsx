73c73d2c4e7a9dcad4012861ca388147d446237a
flaviana <flavicastelocc@gmail.com>
2024-05-30 15:12:04 -0300

    adc imagem por link

--- src/components/Register/index.jsx
+++ src/components/Register/index.jsx
@@ -22,7 +22,7 @@ export default function Register({ handleClick }) {
           <TextComponent text='Cadastre-se' align='justify' sizeResponsive='1.5rem' size='1.5rem' />
           <FormRegister />
         </ContainerForm>
-        <ContainerImg> <ImageStyle src="src/assets/delivery.png" /></ContainerImg>
+        <ContainerImg> <ImageStyle src="https://cdn.discordapp.com/attachments/1118930366728785942/1245801634236203079/delivery.png?ex=665a12af&is=6658c12f&hm=1d50997cc69bf1eedf0fcb1701953378f4fd19277664f35bb018ffd31ef835f1&" /></ContainerImg>
       </ContainerBody>
     </>
   );
